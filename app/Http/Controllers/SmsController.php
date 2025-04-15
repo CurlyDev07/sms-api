76fa59980fdc6bd4ca241cab5c26ddb05437c8fc
curlydev07 <reggie.frias1105@gmail.com>
2025-04-15 19:27:56 +0800

    a

--- app/Http/Controllers/SmsController.php
+++ app/Http/Controllers/SmsController.php
@@ -10,23 +10,13 @@ class SmsController extends Controller
 {
     public function sendSms(Request $request)
     {
-        // Validate the request
-        // $validator = Validator::make($request->all(), [
-        //     'contact_number' => 'required|digits:11',  // Ensure the number is 11 digits (for Philippine numbers)
-        //     'message' => 'required|string|max:160',    // Validate the message is a string with max 160 characters
-        // ]);
-
-        // If validation fails, return error response
-        if ($validator->fails()) {
-            return response()->json([
-                'message' => 'Validation failed',
-                'errors' => $validator->errors()
-            ], 422);
-        }
+        /// Directly access request data
+        $contactNumber = $request->input('contact_number');
+        $message = $request->input('message');
 
         // Call the SmsService to send the message
         $smsService = new SmsService();
-        $smsService->infoTextSend($request->contact_number, $request->message);
+        $smsService->infoTextSend($contactNumber, $message);
 
         return response()->json([
             'message' => 'SMS sent successfully!'
