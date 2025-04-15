2b5a977e156bfd2da286ae7ee746ffbb821ab24f
curlydev07 <reggie.frias1105@gmail.com>
2025-04-15 20:37:14 +0800

    a

--- database/seeders/CustomerInfoSeeder.php
+++ database/seeders/CustomerInfoSeeder.php
@@ -21,58 +21,47 @@ class CustomerInfoSeeder extends Seeder
         DB::table('customer_infos')->insert([
             [
                 'name' => 'John Doe',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime, // Now
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Jane Smith',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(1), // 1 minute later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Mark Johnson',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(2), // 2 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Emily Davis',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(3), // 3 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Lucas White',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(4), // 4 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Sophia Brown',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(5), // 5 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Isabella Green',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(6), // 6 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Mason Black',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(7), // 7 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Ava White',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(8), // 8 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Liam Blue',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(9), // 9 minutes later
+                'contact_number' => '09550090156'
             ],
             [
                 'name' => 'Olivia Yellow',
-                'contact_number' => '09550090156',
-                'order_created_at' => $currentTime->addMinutes(10), // 10 minutes later
+                'contact_number' => '09550090156'
             ]
         ]);
     }
