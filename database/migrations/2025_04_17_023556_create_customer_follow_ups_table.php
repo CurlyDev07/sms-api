a151ed74e665c9f500f257950a106cfe2a2adbd9
curlydev07 <reggie.frias1105@gmail.com>
2025-04-17 10:43:45 +0800

    a

--- database/migrations/2025_04_17_023556_create_customer_follow_ups_table.php
+++ database/migrations/2025_04_17_023556_create_customer_follow_ups_table.php
@@ -15,6 +15,10 @@ class CreateCustomerFollowUpsTable extends Migration
     {
         Schema::create('customer_follow_ups', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('contact_number');
+            $table->foreignId('sms_message_id')->constrained('sms_messages')->onDelete('cascade');
+            $table->integer('interval');
             $table->timestamps();
         });
     }
