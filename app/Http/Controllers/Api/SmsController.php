4fb9e4a317dc164dc5547413d9c08109bc6df89e
curlydev07 <reggie.frias1105@gmail.com>
2025-04-17 10:04:44 +0800

    a

--- app/Http/Controllers/Api/SmsController.php
+++ app/Http/Controllers/Api/SmsController.php
@@ -2,10 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\CustomerInfo;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class SmsController extends Controller
 {
-    //
+    public function customer_info(Request $request){
+        $request->validate([
+            'name' => 'required|string',
+            'contact_number' => 'required|string',
+        ]);
+    
+        $customer = CustomerInfo::create($request->only('name', 'contact_number'));
+    }
 }
