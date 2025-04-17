ac32c74bb991064f408ccf25a4577c114e69e725
curlydev07 <reggie.frias1105@gmail.com>
2025-04-17 11:42:49 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -26,21 +26,26 @@ Route::get('/', function () {
     
         $scheduledTime = $createdAt->addMinutes($intervalMinutes);
         $now = Carbon::now();
-    
-        if ($now->greaterThanOrEqualTo($scheduledTime)) {
-            echo "✅ Time to send SMS to {$scheduledTime}\n <br>'";
-            echo "{$followUp->smsMessage->message_name}\n <br>'";
-            echo "{$followUp->smsMessage->message}\n <br>'";
-            echo "{$followUp->smsMessage->interval}\n <br>'";
-            echo "<hr>";
-            echo "{$followUp->customerInfo->name}\n <br>'";
-            echo "{$followUp->customerInfo->contact_number}\n <br>'";
-            echo '<br>';
-            // You can now trigger SMS or mark as sent
-        } else {
-            echo "⏳ Not yet time for {$followUp->contact_number}\n <br>'";
-            echo '<br>';
-        }
+
+        $diffInMinutes = $now->diffInMinutes($scheduledTime, false); // Use false for absolute difference
+
+        echo " {$diffInMinutes}\n <br>'";
+
+
+        // if ($now->greaterThanOrEqualTo($scheduledTime)) {
+        //     echo "✅ Time to send SMS to {$scheduledTime}\n <br>'";
+        //     echo "{$followUp->smsMessage->message_name}\n <br>'";
+        //     echo "{$followUp->smsMessage->message}\n <br>'";
+        //     echo "{$followUp->smsMessage->interval}\n <br>'";
+        //     echo "<hr>";
+        //     echo "{$followUp->customerInfo->name}\n <br>'";
+        //     echo "{$followUp->customerInfo->contact_number}\n <br>'";
+        //     echo '<br>';
+        //     // You can now trigger SMS or mark as sent
+        // } else {
+        //     echo "⏳ Not yet time for {$followUp->contact_number}\n <br>'";
+        //     echo '<br>';
+        // }
     }
 
 
