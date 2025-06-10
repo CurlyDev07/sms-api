683f137dcb5aed1655b552706e045ec06bcdbffa
curlydev07 <reggie.frias1105@gmail.com>
2025-06-10 15:47:28 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-use App\Models\CustomerFollowUp;
-use Illuminate\Support\Carbon;
-
 Route::get('/', function () {
-    dd('test');
-    return view('welcome');
+    return '✅ Laravel custom route is working!';
 });
-
-
