a24fe2fd3ed162fd940ffe1f75b1c598a3e90029
curlydev07 <reggie.frias1105@gmail.com>
2025-06-10 17:30:28 +0800

    a

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    dd('ssssss');
-    return '✅ Laravel custom route is working!';
-});
-
-
-Route::get('/test', function () {
-    return 'Route working!';
-});
+dd('test');
